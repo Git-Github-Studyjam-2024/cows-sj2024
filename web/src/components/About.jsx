8d352dc0acda9e6d3b2297928ee6908a1d4424c1
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-03-04 12:43:52 +0700

    Fix: Responsive design in About.jsx

--- web/src/components/About.jsx
+++ web/src/components/About.jsx
@@ -50,51 +50,58 @@ const persons = [
 
 const About = () => {
   return (
-    <div className="min-h-screen  px-12 flex bg-white">
-      <div className="h-screen w-full py-48  flex items-center justify-center text-4xl text-green-900 ">
-        {persons.map(
-          (
-            { img, social_medias, name, description, title, background },
-            index
-          ) => {
-            return (
-              <div className={`w-full h-full flex ${background} rounded-2xl`}>
-                <div className="flex items-center  justify-center w-full ">
-                  <section className=" flex flex-col lg:flex-row justify-between w-full px-48">
-                    <Image
-                      src={img}
-                      className="mask mask-squircle max-w-sm rounded-lg shadow-2xl"
-                    />
-                    <div className="w-full px-16 ull flex flex-col gap-4">
-                      <h1 className="text-5xl font-bold">{name}</h1>
-                      <p className="py-2 italic">{title}</p>
-                      <span className="py-2 text-2xl">{description}</span>
-
-                      <div className="flex gap-2 ">
-                        {social_medias.map(
-                          ({ link, icon, background }, index) => {
-                            return (
-                              <button
-                                onClick={() => {
-                                  // Open Facebook
-                                  window.open(link, "_blank");
-                                }}
-                                className={`btn ${background}`}
-                              >
-                                {icon}
-                              </button>
-                            );
-                          }
-                        )}
-                      </div>
-                    </div>
-                  </section>
+    <div className="min-h-screen bg-white flex flex-col overflow-auto w-full gap-4 px-4 py-4   ">
+      {persons.map(
+        (
+          { img, social_medias, name, description, title, background },
+          index
+        ) => {
+          return (
+            <div
+              className={`w-full h-screen flex ${background} flex-col items-center justify-center  p-8 rounded-[2rem]  lg:flex-row mask md:gap-24 gap-4  relative overflow-hidden`}
+            >
+              <div className="bg-white shadow-xl p-1 mask mask-squircle">
+                <Image
+                  src={img}
+                  alt="avatar"
+                  width={300}
+                  height={300}
+                  className="mask mask-squircle"
+                />
+              </div>
+              <div className="flex flex-col gap-4 z-[1]">
+                <h1
+                  className={`text-5xl font-bold text-center md:text-left bg-white ${(() => {
+                    return `text-${background}`;
+                  })()} py-4 px-4 rounded-xl`}
+                >
+                  {name}
+                </h1>
+                <p className="text-center italic text-4xl text-white font-bold md:text-left">
+                  {title}
+                </p>
+                <p className="text-center md:text-left text-2xl">
+                  {description}
+                </p>
+                <div className="flex items-center gap-4 justify-center md:justify-start">
+                  {social_medias.map(({ link, icon, background }, index) => {
+                    return (
+                      <button
+                        onClick={() => {
+                          window.open(link, "_blank");
+                        }}
+                        className={`btn ${background}`}
+                      >
+                        {icon}
+                      </button>
+                    );
+                  })}
                 </div>
               </div>
-            );
-          }
-        )}
-      </div>
+            </div>
+          );
+        }
+      )}
     </div>
   );
 };
